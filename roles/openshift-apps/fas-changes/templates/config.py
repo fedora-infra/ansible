29f4c6adf6284bb30f979ba2289bebb87727166b
Aurélien Bompard <aurelien@bompard.org>
2021-02-05 17:40:42 +0100

    fas-changes: access key
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/openshift-apps/fas-changes/templates/config.py
+++ roles/openshift-apps/fas-changes/templates/config.py
@@ -2,6 +2,8 @@
 # This is the config file for FAS Changes as intended to be used in OpenShift
 #
 
+ACCESS_KEY = "only-for-fas2ipa"
+
 DB_NAME = "fas2"
 DB_HOST = "db-fas{{ env_suffix }}"
 DB_USER = "fasreadonly"
