5db769751c35d52e8dc1581dacc222f822aae911
Stephen Coady <scoady@redhat.com>
2021-03-18 11:43:37 +0000

    add rollback playbook
    Signed-off-by: Stephen Coady <scoady@redhat.com>

---
+++ playbooks/manual/fas-readonly/rollback.sql
@@ -0,0 +1,11 @@
+--
+-- Make fas writable again
+--
+
+
+GRANT ALL PRIVILEGES on people to fas;
+GRANT ALL PRIVILEGES on groups to fas;
+GRANT ALL PRIVILEGES on group_roles to fas;
+GRANT ALL PRIVILEGES on person_roles to fas;
+GRANT ALL PRIVILEGES on person_roles_fcpa to fas;
+GRANT ALL PRIVILEGES on bugzilla_queue to fas;
