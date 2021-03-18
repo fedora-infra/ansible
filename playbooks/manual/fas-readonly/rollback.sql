4a9cb0316c7721748d44d2977faa94b07a4d32ce
Mark O'Brien <markobri@redhat.com>
2021-03-18 11:54:49 +0000

    fix typo on fpca

--- playbooks/manual/fas-readonly/rollback.sql
+++ playbooks/manual/fas-readonly/rollback.sql
@@ -7,5 +7,5 @@ GRANT ALL PRIVILEGES on people to fas;
 GRANT ALL PRIVILEGES on groups to fas;
 GRANT ALL PRIVILEGES on group_roles to fas;
 GRANT ALL PRIVILEGES on person_roles to fas;
-GRANT ALL PRIVILEGES on person_roles_fcpa to fas;
+GRANT ALL PRIVILEGES on person_roles_fpca to fas;
 GRANT ALL PRIVILEGES on bugzilla_queue to fas;
