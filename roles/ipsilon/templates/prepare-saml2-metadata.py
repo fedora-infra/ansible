fc759fd4470ad12d238c9fae0cf2e45fe6e53452
Aurélien Bompard <aurelien@bompard.org>
2021-03-24 18:31:34 +0100

    Add the ipsilon script to generate the metadata
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ roles/ipsilon/templates/prepare-saml2-metadata.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from ipsilon.providers.saml2idp import IdpMetadataGenerator, Certificate
+from datetime import timedelta
+cert = Certificate()
+cert.import_cert('/etc/ipsilon/root/saml2/idp.crt', '/etc/ipsilon/root/saml2/idp.key')
+#meta = IdpMetadataGenerator('https://id{{ env_suffix }}.fedoraproject.org', cert, timedelta(3600))
+meta = IdpMetadataGenerator('https://id{{ env_suffix }}.fedoraproject.org', cert)
+meta.output('/etc/ipsilon/root/saml2/metadata.xml')
