3ac327e4ff6a1693f2854df04785802a1f0a1a40
Kevin Fenzi <kevin@scrye.com>
2021-03-24 15:54:27 -0700

    ircbot: clean up typo in fedmsg-irc
    Signed-off-by: Kevin Fenzi <kevin@scrye.com>

--- roles/fedmsg/irc/templates/ircbot.py
+++ roles/fedmsg/irc/templates/ircbot.py
@@ -680,7 +680,6 @@ config = dict(
                 topic=[
                     '^((?!(pagure)).)*$',
                 ],
-                body=[
                     body=['^((?!(fedora-podcast)).)*$'],
                 ],
             ),
