809635c92375d8e107ecc425380db55e5394e1d1
Aurélien Bompard <aurelien@bompard.org>
2021-04-30 10:28:37 +0200

    Improve the IPA backup process
    Fixes: https://pagure.io/fedora-infrastructure/issue/9916
    
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ roles/ipa/server/files/data-only-backup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Run data only backups daily and only keep a certain number of old backups.
+
+KEEP=14
+
+ls -1d /var/lib/ipa/backup/ipa-data-* | head -n -${KEEP} | xargs rm -rf
+/usr/sbin/ipa-backup --data --online --quiet
