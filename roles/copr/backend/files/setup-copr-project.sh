5e9ee1e814ad3e180ea52e65ffe46f146a828a45
Pavel Raiskup <praiskup@redhat.com>
2021-08-05 15:32:12 +0200

    copr-be: copr-ping: setup both prod/stg instances

--- roles/copr/backend/files/setup-copr-project.sh
+++ roles/copr/backend/files/setup-copr-project.sh
@@ -8,8 +8,8 @@ project=@copr/copr-ping
 pkgname=copr-ping
 
 build_deps=()
-copr_cmd=(
-    copr edit-package-custom "$project" \
+copr_args=(
+    edit-package-custom "$project" \
         --webhook-rebuild on \
         --script "$script" \
         --script-chroot "fedora-latest-x86_64" \
@@ -17,4 +17,11 @@ copr_cmd=(
         --max-builds 10
 )
 
-"${copr_cmd[@]}" --name "$pkgname"
+for instance in copr dev-copr; do
+    config=~/.config/"$instance"
+    test -f "$config" || {
+        echo "can not setup $instance"
+        continue
+    }
+    copr --config "$config" "${copr_args[@]}" --name "$pkgname"
+done
