0ee9fe41bd7ae5b7f66d49040497e4053b93f7d3
Aurélien Bompard <aurelien@bompard.org>
2021-08-12 17:15:07 +0200

    Adjust self-referencing URL
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/openshift-apps/datagrepper/templates/datagrepper.cfg.py
+++ roles/openshift-apps/datagrepper/templates/datagrepper.cfg.py
@@ -2,6 +2,6 @@
 # This is the config file for Datagrepper as intended to be used in OpenShift
 #
 
-APP_PATH = "https://apps{{ env_suffix }}.fedoraproject.org/datagrepper"
+APP_PATH = "https://apps{{ env_suffix }}.fedoraproject.org/datagrepper2"
 DEFAULT_QUERY_DELTA = 3600
 DATANOMMER_SQLALCHEMY_URL = "postgresql://{{ datanommerDBUser }}:{{ datanommerDBPassword }}@db-datanommer01{{ env_suffix }}.iad2.fedoraproject.org/datanommer2"
