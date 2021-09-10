a09a1c9b4e26e5f6db858b68308070f92c653384
Ryan Lerch <rlerch@redhat.com>
2021-09-10 19:24:18 +1000

    badges-stg: update datanommer.models hotfix
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- roles/badges/backend/templates/datanommer.py
+++ roles/badges/backend/templates/datanommer.py
@@ -1,6 +1,6 @@
 config = {
      {% if env == "staging" %}
-    'datanommer.sqlalchemy.url': 'postgresql://{{datanommerDBUser}}:{{datanommerDBPassword}}@db-datanommer.stg.iad2.fedoraproject.org/datanommer',
+    'datanommer.sqlalchemy.url': 'postgresql://{{datanommerDBUser}}:{{datanommerDBPassword}}@db-datanommer.stg.iad2.fedoraproject.org/datanommer2',
     {% else %}
     'datanommer.sqlalchemy.url': 'postgresql://{{datanommerDBUser}}:{{datanommerDBPassword}}@db-datanommer/datanommer',
     {% endif %}
