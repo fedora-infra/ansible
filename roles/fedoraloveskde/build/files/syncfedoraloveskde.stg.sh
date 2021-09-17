66ce205d8a6e78bdca03fd300ec0cb1e91621f65
Timothée Ravier <tim@siosm.fr>
2021-09-18 01:22:11 +0200

    roles/fedoraloveskde: Use hard git resets with an origin
    For fedoraloveskde.org, we currently directly push the pre-built website
    into the repo branches until we figure out a better approach. Thus the
    hisotry does not matter at all here and we just need the latest commit
    fetched and switched to. We can thus directly fetch changes and reset,
    specify the commit we want to use.
    
    Signed-off-by: Timothée Ravier <tim@siosm.fr>

--- roles/fedoraloveskde/build/files/syncfedoraloveskde.stg.sh
+++ roles/fedoraloveskde/build/files/syncfedoraloveskde.stg.sh
@@ -8,6 +8,5 @@ fi
 cd /srv/web/fedoraloveskde.org
 
 /usr/bin/git clean -q -fdx || exit 1
-/usr/bin/git reset -q --hard || exit 1
-/usr/bin/git checkout -q staging || exit 1
-/usr/bin/git pull -q --ff-only || exit 1
+/usr/bin/git fetch -q || exit 1
+/usr/bin/git reset -q --hard origin/staging || exit 1
