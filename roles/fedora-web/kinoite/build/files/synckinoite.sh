4d43b7e377207f27b6110e29b925a25973c194d8
Timothée Ravier <tim@siosm.fr>
2021-09-18 17:31:27 +0200

    kinoite.fedoraproject.org: Add site and pipeline
    Signed-off-by: Timothée Ravier <tim@siosm.fr>

---
+++ roles/fedora-web/kinoite/build/files/synckinoite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -d  /srv/web/kinoite.fedoraproject.org/.git ]
+then
+    /usr/bin/git clone -q https://pagure.io/fedora-kde/kinoite-site /srv/web/kinoite.fedoraproject.org
+fi
+
+cd /srv/web/kinoite.fedoraproject.org
+
+/usr/bin/git clean -q -fdx || exit 1
+/usr/bin/git fetch -q || exit 1
+/usr/bin/git reset -q --hard origin/production || exit 1
