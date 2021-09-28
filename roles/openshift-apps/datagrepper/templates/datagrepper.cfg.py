de38d9d65ac05f6531f1517a0af15b0bea3a4281
Aurélien Bompard <aurelien@bompard.org>
2021-09-28 18:25:54 +0200

    Datagrepper: add live/ready probes and CSP
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/openshift-apps/datagrepper/templates/datagrepper.cfg.py
+++ roles/openshift-apps/datagrepper/templates/datagrepper.cfg.py
@@ -5,3 +5,7 @@
 APP_PATH = "https://apps{{ env_suffix }}.fedoraproject.org/datagrepper2"
 DEFAULT_QUERY_DELTA = 3600
 DATANOMMER_SQLALCHEMY_URL = "postgresql://{{ datanommerDBUser }}:{{ (env == 'production')|ternary(datanommerDBPassword, datanommer_stg_db_password) }}@db-datanommer01{{ env_suffix }}.iad2.fedoraproject.org/datanommer2"
+
+# Only allow ajax/websockets connections back to our domains.
+# https://github.com/fedora-infra/datagrepper/pull/192
+CONTENT_SECURITY_POLICY = "connect-src https://*.fedoraproject.org wss://*.fedoraproject.org"
