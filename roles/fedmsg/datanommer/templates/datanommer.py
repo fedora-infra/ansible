726a7887215b0be07f9ccbfc9e1a98d3958ba61b
Aurélien Bompard <aurelien@bompard.org>
2022-01-17 15:36:17 +0100

    Retire the old datanommer & datagrepper
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/fedmsg/datanommer/templates/datanommer.py
+++
@@ -1,9 +0,0 @@
-# Configuration for the datanommer consumer.  A plugin for the fedmsg-hub process.
-config = {
-    'datanommer.enabled': True,
-{% if env == "production" %}
-    'datanommer.sqlalchemy.url': 'postgresql://{{ datanommerDBUser }}:{{ datanommerDBPassword }}@db-datanommer01.iad2.fedoraproject.org/datanommer',
-{% else %}
-    'datanommer.sqlalchemy.url': 'postgresql://{{ datanommerDBUser }}:{{ datanommer_stg_db_password }}@db-datanommer01.stg.iad2.fedoraproject.org/datanommer',
-{% endif %}
-}
