6b8b45c4353b00d08bc102e16f7fe04bbc96be65
Aurélien Bompard <aurelien@bompard.org>
2022-01-17 13:19:12 +0100

    Move datagrepper2 to prod
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/openshift-apps/datagrepper/templates/datagrepper.cfg.py
+++ roles/openshift-apps/datagrepper/templates/datagrepper.cfg.py
@@ -2,7 +2,7 @@
 # This is the config file for Datagrepper as intended to be used in OpenShift
 #
 
-APP_PATH = "https://apps{{ env_suffix }}.fedoraproject.org/datagrepper2"
+APP_PATH = "https://apps{{ env_suffix }}.fedoraproject.org/datagrepper"
 DEFAULT_QUERY_DELTA = 3600
 DATANOMMER_SQLALCHEMY_URL = "postgresql://{{ datanommerDBUser }}:{{ (env == 'production')|ternary(datanommerDBPassword, datanommer_stg_db_password) }}@db-datanommer{{ (env == 'production')|ternary('02', '01') }}{{ env_suffix }}.iad2.fedoraproject.org/datanommer2"
 
