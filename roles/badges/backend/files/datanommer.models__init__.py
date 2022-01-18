4f3a43253db18f87eca6827136ac8c402494c7af
Aurélien Bompard <aurelien@bompard.org>
2022-01-18 10:20:34 +0100

    Update the badges config for the new datanommer, again
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/badges/backend/files/datanommer.models__init__.py
+++ roles/badges/backend/files/datanommer.models__init__.py
@@ -49,13 +49,8 @@ from sqlalchemy import (
 )
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import (
-    declarative_base,
-    relationship,
-    scoped_session,
-    sessionmaker,
-    validates,
-)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, scoped_session, sessionmaker, validates
 from sqlalchemy.sql import operators
 
 
--- roles/badges/backend/templates/datanommer.py
+++ roles/badges/backend/templates/datanommer.py
@@ -2,6 +2,6 @@ config = {
      {% if env == "staging" %}
     'datanommer.sqlalchemy.url': 'postgresql://{{datanommerDBUser}}:{{datanommer_stg_db_password}}@db-datanommer01.stg.iad2.fedoraproject.org/datanommer2',
     {% else %}
-    'datanommer.sqlalchemy.url': 'postgresql://{{datanommerDBUser}}:{{datanommerDBPassword}}@db-datanommer/datanommer',
+    'datanommer.sqlalchemy.url': 'postgresql://{{datanommerDBUser}}:{{datanommerDBPassword}}@db-datanommer02/datanommer2',
     {% endif %}
 }
