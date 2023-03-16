c5d9b837b22dbf67f6cb91faf664119987dd7368
Lukas Holecek <hluk@email.cz>
2023-03-16 10:57:52 +0100

    waiverdb: Fix OIDC redirect URI
    The wrong redirect URL in OIDC requests is for some reason http://..
    
    See: https://pagure.io/fedora-infrastructure/issue/11179

--- roles/openshift-apps/waiverdb/templates/settings.py
+++ roles/openshift-apps/waiverdb/templates/settings.py
@@ -2,10 +2,12 @@
 DATABASE_URI = 'postgresql+psycopg2://waiverdb@db01.stg.iad2.fedoraproject.org:5432/waiverdb'
 RESULTSDB_API_URL = 'https://resultsdb.stg.fedoraproject.org/api/v2.0'
 CORS_URL = 'https://bodhi.stg.fedoraproject.org'
+OVERWRITE_REDIRECT_URI = 'https://waiverdb.stg.fedoraproject.org/oidc_callback'
 {% else %}
 DATABASE_URI = 'postgresql+psycopg2://waiverdb@db01.iad2.fedoraproject.org:5432/waiverdb'
 RESULTSDB_API_URL = 'https://resultsdb.fedoraproject.org/api/v2.0'
 CORS_URL = 'https://bodhi.fedoraproject.org'
+OVERWRITE_REDIRECT_URI = 'https://waiverdb.fedoraproject.org/oidc_callback'
 {% endif %}
 MESSAGE_BUS_PUBLISH = True
 AUTH_METHOD = 'OIDC'
