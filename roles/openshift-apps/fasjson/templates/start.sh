47fe3157bc9586446faf87db4b7dc571353027d5
Aurélien Bompard <aurelien@bompard.org>
2023-06-06 12:45:07 +0200

    FASJSON: include build scripts in the source code
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/openshift-apps/fasjson/templates/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-rm -rf /httpdir/*
-mkdir /httpdir/run/ /httpdir/run/ccaches/
-ln -s /etc/httpd/modules /httpdir/modules
-truncate --size=0 /httpdir/access.log /httpdir/error.log
-tail -qf /httpdir/access.log /httpdir/error.log &
-exec httpd -f /etc/fasjson/httpd.conf -DFOREGROUND -DNO_DETACH
