bae7c13024b341a473a839aa0c538907df642cb0
Pedro Moura <pmoura@redhat.com>
2023-07-11 19:03:21 -0300

    Added quotes to avoid errors due to version of find
    Signed-off-by: Pedro Moura <pmoura@redhat.com>

--- roles/web-data-analysis/files/compress_logs.sh
+++ roles/web-data-analysis/files/compress_logs.sh
@@ -2,4 +2,4 @@
 
 LOGS_PATH='/mnt/fedora_stats/combined-http'
 
-find $LOGS_PATH/20?? -type f -name '*.log' -not -path $LOGS_PATH/`date +%Y`/`date +%m`/* -exec pxz -z -T 5 {} \;
+find $LOGS_PATH/20?? -type f -name '*.log' -not -path "$LOGS_PATH/`date +%Y`/`date +%m`/*" -exec pxz -z -T 5 {} \;
