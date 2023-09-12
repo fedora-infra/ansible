5107ba9482f4c42b1a5d63faa39f78a4f6117913
Kevin Fenzi <kevin@scrye.com>
2023-09-12 15:43:01 -0700

    waiverdb: try and adjust scopes for staging as a test
    Signed-off-by: Kevin Fenzi <kevin@scrye.com>

--- roles/openshift-apps/waiverdb/templates/settings.py
+++ roles/openshift-apps/waiverdb/templates/settings.py
@@ -3,15 +3,16 @@ DATABASE_URI = 'postgresql+psycopg2://waiverdb@db01.stg.iad2.fedoraproject.org:5
 RESULTSDB_API_URL = 'https://resultsdb.stg.fedoraproject.org/api/v2.0'
 CORS_URL = 'https://bodhi.stg.fedoraproject.org'
 OVERWRITE_REDIRECT_URI = 'https://waiverdb.stg.fedoraproject.org/oidc_callback'
+OIDC_REQUIRED_SCOPE = 'https://waiverdb.stg.fedoraproject.org/oidc/create-waiver'
 {% else %}
 DATABASE_URI = 'postgresql+psycopg2://waiverdb@db01.iad2.fedoraproject.org:5432/waiverdb'
 RESULTSDB_API_URL = 'https://resultsdb.fedoraproject.org/api/v2.0'
 CORS_URL = 'https://bodhi.fedoraproject.org'
 OVERWRITE_REDIRECT_URI = 'https://waiverdb.fedoraproject.org/oidc_callback'
+OIDC_REQUIRED_SCOPE = 'https://waiverdb.fedoraproject.org/oidc/create-waiver'
 {% endif %}
 MESSAGE_BUS_PUBLISH = True
 AUTH_METHOD = 'OIDC'
-OIDC_REQUIRED_SCOPE = 'https://waiverdb.fedoraproject.org/oidc/create-waiver'
 OIDC_CLIENT_SECRETS = '/etc/secret/client_secrets.json'
 OIDC_USERNAME_FIELD = 'sub'
 PREFERRED_URL_SCHEME='https'
