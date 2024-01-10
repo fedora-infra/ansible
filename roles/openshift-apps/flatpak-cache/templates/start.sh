9f3c71793de4a55f397a5832fe6187c64d15a31b
Leo Puvilland <leo@craftcat.dev>
2024-01-10 15:12:51 -0800

    flatpak-cache: Set permissions of PVC to squid user and generate swap
    Signed-off-by: Leo Puvilland <leo@craftcat.dev>

--- roles/openshift-apps/flatpak-cache/templates/start.sh
+++ roles/openshift-apps/flatpak-cache/templates/start.sh
@@ -1,3 +1,3 @@
-exec /sbin/squid
-  --foreground 
-  -f /etc/squid/squid.conf
+exec /usr/lib64/squid/security_file_certgen -M 4096 -c -s /var/spool/squid/ssl_db && \
+  /sbin/squid -z && \
+  /sbin/squid --foreground -f /etc/squid/squid.conf
