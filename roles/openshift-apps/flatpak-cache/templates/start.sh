841ff283aab5d17de9c1805d46be8918bcac31a3
Leo Puvilland <leo@craftcat.dev>
2024-01-11 11:17:19 -0800

    flatpak-cache: switch to /tmp for containing the certificate cache
    Signed-off-by: Leo Puvilland <leo@craftcat.dev>

--- roles/openshift-apps/flatpak-cache/templates/start.sh
+++ roles/openshift-apps/flatpak-cache/templates/start.sh
@@ -1,3 +1,4 @@
-exec /usr/lib64/squid/security_file_certgen -c -s /var/spool/squid/ssl_db -M 4096 && \
+exec /bin/mkdir -p /tmp/ssl_db && \
+  /usr/lib64/squid/security_file_certgen -c -s /tmp/ssl_db -M 4096 && \
   /sbin/squid -z && \
   /sbin/squid --foreground -f /etc/squid/squid.conf
