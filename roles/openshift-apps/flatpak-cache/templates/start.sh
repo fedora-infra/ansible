c6cd7efed4fecb6b170e85399a225cf4e53f1fc5
Kevin Fenzi <kevin@scrye.com>
2024-01-11 11:25:15 -0800

    flatpak-cache: no need to make ssl_db dir
    Signed-off-by: Kevin Fenzi <kevin@scrye.com>

--- roles/openshift-apps/flatpak-cache/templates/start.sh
+++ roles/openshift-apps/flatpak-cache/templates/start.sh
@@ -1,4 +1,3 @@
-exec /bin/mkdir -p /tmp/ssl_db && \
-  /usr/lib64/squid/security_file_certgen -c -s /tmp/ssl_db -M 4096 && \
+exec /usr/lib64/squid/security_file_certgen -c -s /tmp/ssl_db -M 4096 && \
   /sbin/squid -z && \
   /sbin/squid --foreground -f /etc/squid/squid.conf
