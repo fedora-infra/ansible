1c2e14769c949b22526a173ee8ca503580501548
Kevin Fenzi <kevin@scrye.com>
2024-02-06 12:12:35 -0800

    koji / hub: deny any tagging for draft builds currently
    Signed-off-by: Kevin Fenzi <kevin@scrye.com>

--- roles/ipa/server/files/data-only-backup.sh
+++ roles/ipa/server/files/data-only-backup.sh
@@ -2,7 +2,7 @@
 
 # Run data only backups daily and only keep a certain number of old backups.
 
-KEEP=14
+KEEP=7
 
 ls -1d /var/lib/ipa/backup/ipa-data-* | head -n -${KEEP} | xargs rm -rf
 /usr/sbin/ipa-backup --data --online --quiet
