5a972ede1f980999f9e69609305aa341a0489980
Aurélien Bompard <aurelien@bompard.org>
2024-02-22 18:55:54 +0100

    MM: add a cron job to get the access statistics
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

---
+++ roles/openshift-apps/mirrormanager/templates/create_statistics.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+MIRRORLIST_PROXIES="{% for host in groups['mirrorlist_proxies'] %} {{ host }} {% endfor %}"
+
+MIRRORLIST_LOGDIR="/var/log/mirrormanager"
+MIRRORLIST_LOGFILES="mirrorlist1.service.log mirrorlist2.service.log"
+SSH_KEY="/etc/mirrormanager-secrets/ssh_mirrorlist_proxies.key"
+REMOTE_USER="mirrormanager"
+
+SSH="ssh -i ${SSH_KEY}"
+DATE=`date +%Y%m%d`
+OUTPUT=`mktemp -d`
+
+trap "rm -f ${OUTPUT}/*; rmdir ${OUTPUT}" QUIT TERM INT HUP EXIT
+
+for proxy in ${MIRRORLIST_PROXIES}; do
+	if [ "$1" == "yesterday" ]; then
+		for logfile in ${MIRRORLIST_LOGFILES}; do
+			${SSH} ${REMOTE_USER}@${proxy} "( xzcat ${MIRRORLIST_LOGDIR}/${logfile}-${DATE}.xz | grep -v 127.0.0.1 | gzip -4 )" >> ${OUTPUT}/mirrorlist.log.gz
+		done
+	fi
+	for logfile in ${MIRRORLIST_LOGFILES}; do
+		${SSH} ${REMOTE_USER}@${proxy} "( cat ${MIRRORLIST_LOGDIR}/${logfile} | grep -v 127.0.0.1 | gzip -4 )" >> ${OUTPUT}/mirrorlist.log.gz
+	done
+done
+
+if [ "$1" == "yesterday" ]; then
+	OPTIONS="-o 1"
+else
+	OPTIONS=""
+fi
+
+mm2_mirrorlist-statistics ${OPTIONS} -l ${OUTPUT}/mirrorlist.log.gz
