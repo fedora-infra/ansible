7133b7db35b3f93e81825ca5ef3e15d93614d5fa
Aurélien Bompard <aurelien@bompard.org>
2024-05-23 12:09:30 +0200

    Generate a list of expiration dates for the certs
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/batcave/files/make-rabbitmq-certs-public.sh
+++ roles/batcave/files/make-rabbitmq-certs-public.sh
@@ -2,12 +2,19 @@
 
 BASE_SRC_DIR=/srv/private/ansible/files/rabbitmq
 BASE_DEST_DIR=/var/cache/rabbitmq-certs
+EXPIRATION_FILE=expiration.txt
 
 set -e
 
 for env in staging production; do
-        dest_dir=${BASE_DEST_DIR}/${env}
-        mkdir -p ${dest_dir}
-        cp -a ${BASE_SRC_DIR}/${env}/pki/issued/*.crt ${dest_dir}/
+    dest_dir=${BASE_DEST_DIR}/${env}
+    mkdir -p ${dest_dir}
+    > ${dest_dir}/${EXPIRATION_FILE}
+    for cert in `ls ${BASE_SRC_DIR}/${env}/pki/issued/*.crt`; do
+        cp -a $cert ${dest_dir}/
         chmod 644 ${dest_dir}/*.crt
+        name=`basename $cert .crt`
+        exp_date=`openssl x509 -enddate -noout -dateopt iso_8601 -in $cert | cut -d= -f2`
+        echo -e "$name\t$exp_date" >> ${dest_dir}/${EXPIRATION_FILE}
+    done
 done
