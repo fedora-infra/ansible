6bbe0b70c3373153f6532f1d5b4bd03f4e81a386
Aurélien Bompard <aurelien@bompard.org>
2024-06-13 08:16:25 +0200

    Badges: the flask rewrite is now deployed to prod
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/openshift-apps/badges/templates/wsgi.py
+++ roles/openshift-apps/badges/templates/wsgi.py
@@ -1,12 +1,4 @@
-{% if env == "staging" %}
 from werkzeug.middleware.proxy_fix import ProxyFix
 from tahrir.app import create_app
 application = create_app()
 application.wsgi_app = ProxyFix(application.wsgi_app, x_proto=1, x_host=1)
-{% else %}
-from pyramid.paster import get_app, setup_logging
-ini_path = '/etc/badges/tahrir.ini'
-setup_logging(ini_path)
-
-application = get_app(ini_path, 'main')
-{% endif %}
