d8f7a508f8df1f07d3bbff643c80da7b65d1191c
Aurélien Bompard <aurelien@bompard.org>
2024-09-13 12:12:14 +0200

    MM: adjust primary-mirror-wrapper because ELN has only one version
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- roles/openshift-apps/mirrormanager/templates/primary-mirror-wrapper.sh
+++ roles/openshift-apps/mirrormanager/templates/primary-mirror-wrapper.sh
@@ -76,7 +76,11 @@ elif [ "${1}" ==  "codecs" ]; then
 	FFTL="${CATEGORY}"
 	FILEDATE=${CURDATE}
 else
-	FFTL="/srv/pub/${1}/fullfiletimelist-${1}"
+	if [ "${1}" ==  "eln" ]; then
+		FFTL="/srv/pub/${1}/1/fullfiletimelist-${1}"
+	else
+		FFTL="/srv/pub/${1}/fullfiletimelist-${1}"
+	fi
 	FILEDATE=`stat -c %Z ${FFTL} 2> /dev/null`
 
 	if [ "$?" -eq "1" ]; then
