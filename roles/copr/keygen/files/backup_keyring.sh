99e3ca8b1e774b6732702e8cd761002a7a2bc1d9
Pavel Raiskup <praiskup@redhat.com>
2025-01-13 08:31:08 +0100

    copr-keygen: fix using of backup key
    Do not require the key to be imported in keyring.

--- roles/copr/keygen/files/backup_keyring.sh
+++ roles/copr/keygen/files/backup_keyring.sh
@@ -10,7 +10,7 @@ OUTPUT_FILE="$BACKUP_DIR/copr_keygen_keyring_$(date -I).tar.gz.gpg"
 
 tar --exclude="*agent*" -czPf - "$PATH_TO_KEYRING_DIR" \
     | gpg2 --output "$OUTPUT_FILE".tmp --encrypt \
-           --recipient copr-keygen-backup-key --always-trust \
+           --recipient-file /root/backup_key.asc \
 && mv "$OUTPUT_FILE.tmp" "$OUTPUT_FILE"
 
 # shell pattern matching provides sorted output
