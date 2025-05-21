835a7156c118b9452c07b08c7800997a9e3f3cf8
Kevin Fenzi <kevin@scrye.com>
2025-05-21 16:05:48 -0700

    rdu3: fix ps1
    Signed-off-by: Kevin Fenzi <kevin@scrye.com>

--- roles/base/files/setprodrdu3ps1.sh
+++ roles/base/files/setprodrdu3ps1.sh
@@ -1,3 +1,3 @@
 # Set PS1 based on env
 #
-[[ $- =~ i ]] && PS1="[\u@\h \W]\[$(tput setaf 3)\][PROD-IAD2]\[$(tput sgr0)\]\\$ \[\]"
+[[ $- =~ i ]] && PS1="[\u@\h \W]\[$(tput setaf 3)\][PROD-RDU3]\[$(tput sgr0)\]\\$ \[\]"
