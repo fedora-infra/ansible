fedora-infra/ansible

commit f513e7cbcd845367e42ba2d0c85d19b8f9423e75

Linting python scripts
Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

roles/openshift-apps/kerneltest/templates/wsgi.py
@@ -1,1 +1,0 @@
-from kerneltest.app import APP as application

callback_plugins/logdetail.py
@@ -60,7 +60,7 @@ MSG_FORMAT = "%(now)s\t%(count)s\t%(category)s\t%(name)s\t%(data)s\n"
 def getlogin():
     try:
         user = os.getlogin()
-    except OSError as e:
+    except OSError:
         user = pwd.getpwuid(os.geteuid())[0]
     return user
 

roles/batcave/files/koji_sar.py
@@ -6,7 +6,6 @@ import os
 import json
 import subprocess
 import sys
-import time
 
 import koji
 

roles/batcave/files/oshift_mod.py
@@ -18,7 +18,6 @@ import time
 import traceback
 import json
 
-import base64
 import requests
 
 

roles/bodhi2/backend/files/bodhi-automated-pushes.py
@@ -2,9 +2,7 @@
 from __future__ import print_function
 import sys
 import requests
-import json
 import subprocess
-import logging
 
 req = requests.get('https://bodhi.fedoraproject.org/composes/')
 bodhi_composes = req.json()

roles/cloudstats/files/cloud-image-stat.py
@@ -1,6 +1,6 @@
 from glob import glob
 from datetime import date
-from os import walk,listdir
+from os import listdir
 from os.path import exists
 from bz2 import BZ2File
 import smtplib

roles/collectd/fedmsg-activation/files/fedmsg-map.py
@@ -73,7 +73,7 @@ def scan_all():
     info()
 
     if 'verbose' in sys.argv:
-        import pprint;
+        import pprint
         pprint.pprint(dict(active))
         pprint.pprint(dict(inactive))
 

roles/epylog/files/modules/common_unparsed_mod.py
@@ -16,7 +16,6 @@ import difflib
 from operator import itemgetter
 import tempfile
 import os
-import shutil
 
 ##
 # This is for testing purposes, so you can invoke this from the
@@ -24,7 +23,7 @@ import shutil
 # file.
 #
 sys.path.insert(0, '../py/')
-from epylog import Result, InternalModule
+from epylog import InternalModule
 
 class common_unparsed_mod(InternalModule):
     def __init__(self, opts, logger):

commit 2f22b5edea28a4e370a68e791b38916bdf032ab1

Fix kerneltest wsgi script broken by f513e7c
Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

roles/openshift-apps/kerneltest/templates/wsgi.py
@@ -1,0 +1,1 @@
+from kerneltest.app import APP as application